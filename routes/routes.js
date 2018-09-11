b472f848078f9e9bc5168915fde98eaf938baa1a
rectoria <bastien.rinck@epitech.eu>
2018-09-11 20:18:17 +0200

    test fix

--- routes/routes.js
+++ routes/routes.js
@@ -21,14 +21,15 @@ module.exports = function(router, databases){
 
     router.use('/api/username/:userName', (req, res)=> {
         let url = `https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.userName}`;
-        databases.Summoner.findAll({where: {
+        databases.Summoner.findOne({where: {
             name: req.params.userName
             }}).then((matches)=> {
                 console.log(matches);
                 if (matches.length) {
                     res.json(matches[0]);
                 } else {
-                    let data = JSON.parse(proccedCall(url));
+                    throw "error";
+                    let data = JSON.parse("");
                     database.Summoner.save({accountId: data.id, account: data.accountId, name: name}).then((user) => {
                         req.json(user);
                     }, (err) => {
@@ -41,7 +42,7 @@ module.exports = function(router, databases){
     });
     router.use('/api/rank/:userid', (req, res)=> {
 	let url = `https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/${req.params.userid}`;
-        databases.Summoner.findAll({where: {
+        databases.Summoner.findOne({where: {
                 name: req.params.userid
             }}).then((matches)=> {
             console.log(matches);

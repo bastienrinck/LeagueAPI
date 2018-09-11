769483180dbb68bd43b429ea6e3f11e59465d32a
Bastien <bastien.rinck@epitech.eu>
2018-09-11 17:41:45 +0200

    [~] fixing and testing cache method

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const https = require('https');
 const key = 'RGAPI-1c469f1b-6b43-4575-a409-e82d068507e9';
 const options = {headers: {'X-Riot-Token': key}};
 
-function proccedCall(url, res) {
+function proccedCall(url) {
     https.get(url, options, (resp)=> {
         let data = '';
 
@@ -11,7 +11,7 @@ function proccedCall(url, res) {
         });
 
         resp.on('end', () => {
-            res.json(data);
+            return data;
         });
     });
 }
@@ -23,9 +23,18 @@ module.exports = function(router, databases){
         let url = `https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.userName}`;
         databases.Summoner.findAll({where: {
             name: req.params.userName
-            }}).then((response)=> {
-                console.log(response);
-                proccedCall(url, res);
+            }}).then((matches)=> {
+                console.log(matches);
+                if (matches.length) {
+                    res.json(matches[0]);
+                } else {
+                    let data = JSON.parse(proccedCall(url));
+                    database.Summoner.save({accountId: data.id, account: data.accountId, name: name}).then((user => {
+                        req.json(user);
+                    }, (err) => {
+                        console.error("Error occurred while saving new instance")
+                    }))
+                }
         }, (err)=> {
                 console.error(err);
         });
@@ -34,9 +43,10 @@ module.exports = function(router, databases){
 	let url = `https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/${req.params.userid}`;
         databases.Summoner.findAll({where: {
                 name: req.params.userid
-            }}).then((response)=> {
-            console.log(response);
-            proccedCall(url, res);
+            }}).then((matches)=> {
+            console.log(matches);
+            let data = proccedCall(url);
+            res.json(data);
         }, (err)=> {
             console.error(err);
         });

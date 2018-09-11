30bcc42e19d0199c641aa4dda98c1819f37086a8
rectoria <bastien.rinck@epitech.eu>
2018-09-11 20:32:54 +0200

    test fix

--- routes/routes.js
+++ routes/routes.js
@@ -32,7 +32,7 @@ module.exports = function(router, databases){
             } else {
                 proccedCall(url).then((data) => {
                     JSON.parse(data);
-                    databases.Summoner.save({accountId: data.id, account: data.accountId, name: name}).then((user) => {
+                    databases.Summoner.save({accountId: data.id, account: data.accountId, name: data.name}).then((user) => {
                         req.json(user);
                     }, (err) => {
                         console.error("Error occurred while saving new instance")

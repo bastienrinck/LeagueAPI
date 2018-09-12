26a0a490fd6b865e6fe88505cb23adf254a6115d
Bastien <bastien.rinck@epitech.eu>
2018-09-12 14:43:26 +0200

    Test

--- routes/routes.js
+++ routes/routes.js
@@ -19,49 +19,50 @@ function proccedCall(url) {
 // routes
 module.exports = function(router, databases){
 
-    router.use('/api/username/:userName', (req, res)=> {
+    router.use('/api/username/:userName', async (req, res) => {
         let url = `https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.userName}`;
-        databases.Summoner.findOne({
-            where: {
-                name: req.params.userName
+        let matches;
+        try {
+            matches = await databases.Summoner.findOne({
+                where: {
+                    name: req.params.userName
+                }
+            });
+        } catch (err) {
+            return res.json("An error occurred while retrieving data from riot API");
+        }
+        if (matches && (Date.now() - matches.dataValues.updatedAt.valueOf()) < 30000) {
+            res.json(matches.dataValues);
+        } else if (matches) {
+            console.log("Refreshing cache");
+            try {
+                let data = await proccedCall(url);
+                let parseData = JSON.parse(data);
+                try {
+                    let user = await
+                    matches.save({id: parseData.id, accountId: parseData.accountId, name: parseData.name});
+                    res.json(user.dataValues);
+                } catch (err) {
+                    return res.json("An error occurred while updating user on database")
+                }
+            } catch (err) {
+                return res.json("An error occurred while retrieving data from database")
             }
-        }).then((matches) => {
-            console.log(Date.now());
-            console.log(matches.dataValues.updatedAt.valueOf());
-            console.log(Date.now() - matches.dataValues.updatedAt.valueOf());
-            console.log((Date.now() - matches.dataValues.updatedAt.valueOf()) < 30000);
-            if (matches && (Date.now() - matches.dataValues.updatedAt.valueOf()) < 30000) {
-                console.log(`${Date.now()} - ${matches.dataValues.updatedAt.valueOf()} = ${Date.now() - matches.dataValues.updatedAt.valueOf()}`);
-                res.json(matches.dataValues);
+        } else {
+            try {
+                let data = await proccedCall(url);
+                let parseData = JSON.parse(data);
+                try {
+                    let user = await databases.Summoner.update({id: parseData.id, accountId: parseData.accountId, name: parseData.name});
+                    res.json(user.dataValues);
+                } catch (err) {
+                    return res.json("An error occurred while updating user on database")
+                }
+            } catch (err) {
+                return res.json("An error occurred while retrieving data from riot API");
             }
-            else if (matches) {
-                console.log("Refreshing cache");
-                proccedCall(url).then((data) => {
-                    let parseData = JSON.parse(data);
-                    matches.save({id: parseData.id, accountId: parseData.accountId, name: parseData.name}).then((user) => {
-                        res.json(user.dataValues);
-                    }, (err) => {
-                        console.error("Error occurred while saving new instance");
-                        console.error(`"Error: " ${err}`);
-                    });
-                }, () => {
-                });
-            } else {
-                proccedCall(url).then((data) => {
-                    let parseData = JSON.parse(data);
-                    databases.Summoner.updatedAt = Date.now();
-                    databases.Summoner.updatedAt.changed('updatedAt', true);
-                    databases.Summoner.update({id: parseData.id, accountId: parseData.accountId, name: parseData.name}).then((user) => {
-                        res.json(user.dataValues);
-                    }, (err) => {
-                        console.error("Error occurred while saving new instance");
-                        console.error(`"Error: " ${err}`);
-                    });
-                }, () => {
-                });
-            }
-        });
-    });
+        }
+});
 
     router.use('/api/rank/:userid', (req, res)=> {
 	let url = `https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/${req.params.userid}`;
